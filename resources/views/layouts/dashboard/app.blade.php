bd2fc630953ad739aaa3c2710dc6f08dd2fc5700
Diaa Abdallah <diaaemad1@gmail.com>
2022-07-16 20:30:12 +0200

    modify layout

--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -229,12 +229,12 @@
 
     @include('partials._session')
 
-    <footer class="main-footer">
+    <footer class="main-footer text-center">
         <div class="pull-right hidden-xs">
             <b>Version</b> 2.4.0
         </div>
-        <strong>Copyright &copy; 2014-2016
-            <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+        <strong>Copyright &copy; 2021-2022
+            <a href="https://adminlte.io">Diaa Abdallah</a>.</strong> All rights
         reserved.
     </footer>
 
